19f001dcfbefb294f36fe93703a8ee3581fa9a01
swara12262 <swaranjalisawant@gmail.com>
2020-07-22 09:25:13 +0530

    Created userrequest model to convert incoming json to Java object

--- mobile-app-ws/src/main/java/com/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/app/ws/ui/controller/UserController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.ws.ui.model.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
+	
 	
 	@GetMapping
 	public String getUser()
@@ -18,7 +22,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser()
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails)
 	{
 		return "Create User called";
 	}
