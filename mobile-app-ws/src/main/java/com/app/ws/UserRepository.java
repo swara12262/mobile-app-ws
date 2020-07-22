3e5f02f6fecde295da05ab3c4e8afa48f7adb26c
swara12262 <swaranjalisawant@gmail.com>
2020-07-22 13:18:51 +0530

    32. Implementing UserRepository class.

---
+++ mobile-app-ws/src/main/java/com/app/ws/UserRepository.java
@@ -0,0 +1,14 @@
+package com.app.ws;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.ws.io.entity.UserEntity;
+
+//UserEntity needs to be persisted and long is the type of Id in userEntity
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+	
+	UserEntity findUserByEmail(String email); 
+
+}
