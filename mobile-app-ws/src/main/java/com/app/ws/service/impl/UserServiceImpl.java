a2d4bfb202967bca995e6e64c6c3bd0794deadc9
swara12262 <swaranjalisawant@gmail.com>
2020-08-01 15:22:08 +0530

    40. Implmementing user sign in request model and load a user using
    username.

--- mobile-app-ws/src/main/java/com/app/ws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/app/ws/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.app.ws.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -47,8 +50,9 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		UserEntity userEntity  = userRepository.findByEmail(username);
+		if(userEntity==null) throw new UsernameNotFoundException("Email id : "+username +"Does not exists in Database");
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
 
 }
