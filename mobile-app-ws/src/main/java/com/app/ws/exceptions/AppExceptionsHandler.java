239a11e07d4ea849cc5b181c74aa5af4db588f1f
swara12262 <swaranjalisawant@gmail.com>
2020-08-15 12:47:26 +0530

    62. Handle a specific exception and return custom error object.

---
+++ mobile-app-ws/src/main/java/com/app/ws/exceptions/AppExceptionsHandler.java
@@ -0,0 +1,24 @@
+package com.app.ws.exceptions;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.app.ws.ui.model.reponse.ErrorMessage;
+
+@ControllerAdvice
+public class AppExceptionsHandler {
+
+	@ExceptionHandler(value= {UserServiceException.class})
+	public ResponseEntity<Object> handleUserServiceExceptions(UserServiceException ex, WebRequest req){
+		ErrorMessage erroMessage=new ErrorMessage(new Date(), ex.getMessage());
+			
+		return new ResponseEntity<>(erroMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+}
